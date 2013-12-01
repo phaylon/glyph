c5b819f9fa925a455274b00f5fa54b2b9117f91a
Robert Sedlacek <rs@474.at>
2013-12-01 20:15:05 +0100

    added list of required versions for deps

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#
+#   Requirements:
+#       gtk+-3.0 (3.4.2)
+#       gio-2.0 (2.32.2)
+#       gee-1.0 (0.6.8 - don't ask me)
+#       gtksourceview-3.0 (3.4.2)
+#       pango (1.30.0)
+#       json-glib-1.0 (0.14.2 - another weird one)
+#
+#   Perl 5 Requirements:
+#       Path-Tiny
+#       Template-Toolkit
+#
 glib-compile-schemas share/schemas
 perl maint/styles/generate.pl
 valac -o glyph --verbose -X -w\
